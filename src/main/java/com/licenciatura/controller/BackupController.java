35189939c563890bf5786957aff13e47ca49adb6
Pablo Sanchez <psanchez@fahce.unlp.edu.ar>
2024-11-29 16:27:31 -0300

    Update BackupController.java

--- src/main/java/com/licenciatura/controller/BackupController.java
+++ src/main/java/com/licenciatura/controller/BackupController.java
@@ -1,1 +1,24 @@
+package com.licenciatura.controller;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+
+@RestController
+public class BackupController {
+
+    @GetMapping("/backup")
+    public ResponseEntity<Resource> downloadBackup() {
+        File file = new File("/data/licenciatura/backup.sql");
+        Resource resource = new FileSystemResource(file);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=backup.sql")
+                .body(resource);
+    }
+}
